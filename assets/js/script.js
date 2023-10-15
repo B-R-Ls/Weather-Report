910d010ba4b832b1c5fec1cdf78856ba15451543
Brendan <brendan.r.le@gmail.com>
2023-10-14 21:02:51 -0500

    added button functionallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var searchinput = $('#search');
 var clickinput = $('#clickinput');
-var citylist = $('#citylist')
+var citylist = $('#citylist');
+var maincard = $('#mainCard');
+var subCards = $('#subCards');
 
 var cities = [];
 
@@ -59,46 +61,80 @@ citylist.on('click', function(event) {
         url: 'http://api.openweathermap.org/geo/1.0/direct?q='+ event.target.lastChild.data +'&appid=e9e30400559d99c2dce675aec0e4ad08',
         method: 'GET',
     }).then(function (response) {
-        // console.log('AJAX Response \n-------------');
-        // console.log(response);
+
         var lat = response[0].lat;
         var lon = response[0].lon;
-        // console.log(lat, lon)
+
         $.ajax({
             url: 'http://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon=' + lon + '&appid=e9e30400559d99c2dce675aec0e4ad08',
             method: 'GET',
         }).then(function (response) {
-            console.log('AJAX Response \n-------------');
-            // console.log(response);
-            console.log(response.list)
+
             for (var i = 0; i < 5; i++ ) {
                 var x = 8 * [i];
-                var temp = response.list[x].main.temp;
+                var temp = Math.round((((response.list[x].main.temp - 273.15) * 9)/5) + 32);
                 var wind = response.list[x].wind.speed;
                 var humid = response.list[x].main.humidity;
-                var weather = response.list[x].weather[0].main
-                console.log(temp, wind, humid, weather);
-                if (i === 0 ) {
+                var weather = response.list[x].weather[0].main;
+                var date = response.list[x].dt_txt
+                console.log(temp, wind, humid, weather, date);
 
+                if (weather === 'Clear') {
+                    weather = '☀️';
+                } else if (weather === 'Rain'){
+                    weather = '🌧️';
                 } else {
+                    weather = '☁️';
+                }
 
+                if (i === 0 ) {
+                    var card = $('<section>')
+                    var header = $('<p>');
+                    var headtemp = $('<p>');
+                    var headwind = $('<p>');
+                    var headhumid = $('<p>');
+
+                    header.text(event.target.lastChild.data + ' ' + date + ' ' + weather);
+                    headtemp.text('Temp: ' + temp + '°F');
+                    headwind.text('Wind: ' + wind + ' MPH');
+                    headhumid.text('Humidity: ' + humid + '%');
+
+                    $(card).addClass('w-90 border border-dark m-3 p-3');
+                    $(header).addClass('h2');
+
+                    maincard.append(card);
+                    card.append(header);
+                    card.append(headtemp);
+                    card.append(headwind);
+                    card.append(headhumid);
+                } else {
+                    var subcard = $('<section>');
+                    var subdate = $('<p>');
+                    var subweather = $('<p>');
+                    var subtemp = $('<p>');
+                    var subwind = $('<p>');
+                    var subhumid = $('<p>');
+
+                    subdate.text(date);
+                    subweather.text(weather);
+                    subtemp.text('Temp: ' + temp + '°F');
+                    subwind.text('Wind: ' + wind + ' MPH');
+                    subhumid.text('Humidity: ' + humid + '%');
+
+                    $(subcard).addClass('col-2 border border-dark m-5 p-3');
+                    $(subdate).addClass('font-weight-bold');
+
+                    subCards.append(subcard);
+                    subcard.append(subdate);
+                    subcard.append(subweather);
+                    subcard.append(subtemp);
+                    subcard.append(subwind);
+                    subcard.append(subhumid);
                 }
             }
         });
     });
-// clear clouds rain 
     
-
 });
-
-
-
-
-
-// citylist.on('click', function(event) {
-//     var element = event.target;
-
-//     if (element === )
-// });
 
 init();
